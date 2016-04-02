87bbc1496549ec985fc30d95a91afb9c47d568c7
qingyuan <ae5656ln@163.com>
2016-04-02 22:52:32 +0800

    单个爬虫配置

---
+++ src/cn/kejso/Template/SpiderConf.java
@@ -0,0 +1,52 @@
+package cn.kejso.Template;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.kejso.Template.ToolEntity.BaseConfig;
+
+public class SpiderConf {
+	
+		//爬虫名称
+		private String  name;
+		
+		//配置
+		private BaseConfig config;
+		
+		//依赖的爬虫名称
+		private String dependname;
+
+		
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public BaseConfig getConfig() {
+			return config;
+		}
+
+		public void setConfig(BaseConfig config) {
+			this.config = config;
+		}
+
+		public String getDependname() {
+			return dependname;
+		}
+
+		public void setDependname(String dependname) {
+			this.dependname = dependname;
+		}
+		
+		
+		//get depend urls
+		public List<String> getDependUrls()
+		{
+			List<String> urls=new ArrayList<String>();
+			
+			return urls;
+		}
+}
