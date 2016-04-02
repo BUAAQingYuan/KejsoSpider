1b7e0ab120decc58da1bbab6b9ee69bf455eea71
qingyuan <ae5656ln@163.com>
2016-04-02 22:52:12 +0800

    爬虫全局配置类

---
+++ src/cn/kejso/Template/ToolEntity/GlobalConfig.java
@@ -0,0 +1,33 @@
+package cn.kejso.Template.ToolEntity;
+
+//配置文件中的全局配置
+public class GlobalConfig {
+	
+	private  String   taskname;
+	
+	//线程数
+	private  int      threadnum;
+	
+	//开启代理
+	private  boolean  enableproxy;
+	
+	public String getTaskname() {
+		return taskname;
+	}
+	public void setTaskname(String taskname) {
+		this.taskname = taskname;
+	}
+	public int getThreadnum() {
+		return threadnum;
+	}
+	public void setThreadnum(int threadnum) {
+		this.threadnum = threadnum;
+	}
+	public boolean isEnableproxy() {
+		return enableproxy;
+	}
+	public void setEnableproxy(boolean enableproxy) {
+		this.enableproxy = enableproxy;
+	}
+	
+}
