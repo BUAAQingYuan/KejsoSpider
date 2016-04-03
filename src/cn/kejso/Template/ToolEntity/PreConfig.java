54397556b5886aed43e86d6b0f70765a223ab698
qingyuan <ae5656ln@163.com>
2016-04-03 23:49:29 +0800

    获得列表页url的预处理

---
+++ src/cn/kejso/Template/ToolEntity/PreConfig.java
@@ -0,0 +1,60 @@
+package cn.kejso.Template.ToolEntity;
+
+import java.util.List;
+
+//获得列表页url
+public class PreConfig extends BaseConfig{
+	
+	//数据表
+	private  String tablename;
+	//字段
+	private  List<String> fields;
+	//unique字段
+	private  String  unique;
+	//preurl
+	private  String  preurl;
+	//prevalue
+	private  String  prevalue;
+	
+	
+	public PreConfig(String preurl,String prevalue,String tablename,List<String> fields,String unique)
+	{
+		this.preurl=preurl;
+		this.prevalue=prevalue;
+		this.tablename=tablename;
+		this.fields=fields;
+		this.unique=unique;
+	}
+	
+	public String getTablename() {
+		return tablename;
+	}
+	public void setTablename(String tablename) {
+		this.tablename = tablename;
+	}
+	public List<String> getFields() {
+		return fields;
+	}
+	public void setFields(List<String> fields) {
+		this.fields = fields;
+	}
+	public String getUnique() {
+		return unique;
+	}
+	public void setUnique(String unique) {
+		this.unique = unique;
+	}
+	public String getPreurl() {
+		return preurl;
+	}
+	public void setPreurl(String preurl) {
+		this.preurl = preurl;
+	}
+	public String getPrevalue() {
+		return prevalue;
+	}
+	public void setPrevalue(String prevalue) {
+		this.prevalue = prevalue;
+	}
+	
+}
