5a3c3f4ce75a23902ff8b19d4b3b40dc5d8b2470
ChengChung <ChengChung@live.cn>
2016-04-15 17:05:24 +0800

    spiderChain 基本功能

--- src/cn/kejso/Spider/BuildSpiderChain.java
+++ src/cn/kejso/Spider/BuildSpiderChain.java
@@ -10,77 +10,116 @@ import cn.kejso.PageProcess.PreListPageProcess;
 import cn.kejso.Pipeline.MysqlPipeline;
 import cn.kejso.Spider.Control.FileCacheOnErrorListener;
 import cn.kejso.Spider.Control.SpiderContainer;
+import cn.kejso.Spider.Control.SpiderContainer.Function;
 import cn.kejso.Template.SpiderConf;
 import cn.kejso.Template.ToolEntity.BaseConfig;
 import cn.kejso.Template.ToolEntity.ContentConfig;
 import cn.kejso.Template.ToolEntity.GlobalConfig;
 import cn.kejso.Template.ToolEntity.ListConfig;
 import cn.kejso.Template.ToolEntity.PreConfig;
+import cn.kejso.Tool.SpiderUtil;
+import cn.kejso.Tool.SqlUtil;
 import cn.kejso.Tool.TemplateConstructor;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.SpiderListener;
 import us.codecraft.webmagic.processor.PageProcessor;
 
 public class BuildSpiderChain {
-	
+
 	private List<SpiderConf> confs;
 	private GlobalConfig global;
 	private SpiderChain chain;
-	
+
+
+
 	public BuildSpiderChain(String configPath) {
-		
-		chain=new SpiderChain();
-		confs=TemplateConstructor.getSpiderConf(configPath);
-		global=TemplateConstructor.getGlobalConf(configPath);
-		
+
+		chain = new SpiderChain();
+		confs = TemplateConstructor.getSpiderConf(configPath);
+		global = TemplateConstructor.getGlobalConf(configPath);
+
 		for (SpiderConf conf : confs) {
-			Spider spider = Spider.create(getPageProcessor(conf))
-									.thread(global.getThreadnum())
-									.addPipeline(new MysqlPipeline(conf));
+			Spider spider = Spider.create(getPageProcessor(conf)).thread(global.getThreadnum())
+					.addPipeline(new MysqlPipeline(conf));
 			spider.setSpiderListeners(getSpiderListeners(conf));
 			SpiderContainer container = new SpiderContainer(spider, conf);
-//			container.AddgetStartUrlHandler(handler);
+			container.AddgetStartUrlHandler(getStartUrlHandler(conf));
 			chain.AddSpiderNode(container);
 		}
-	
+
 	}
-	
-	private List<SpiderListener> getSpiderListeners(SpiderConf conf) {
-		List <SpiderListener> list = new ArrayList<SpiderListener>();
+
+	private Function<Spider, SpiderConf> getStartUrlHandler(SpiderConf conf) {
+
+		Function<Spider, SpiderConf> result = null;
 		
-		if (conf.getConfig() instanceof ContentConfig) {
-			list.add(new FileCacheOnErrorListener(Config.Spider_ErrorDir+global.getTaskname()));
+		if (conf.getDependname() == null) {
+			result = readUrlFromConf;
 		}
-		
+		else if (conf.getDependname() != null) {
+			result = readUrlFromSql;
+		}
+
+		return result;
+	}
+
+	private List<SpiderListener> getSpiderListeners(SpiderConf conf) {
+		List<SpiderListener> list = new ArrayList<SpiderListener>();
+
+		if (conf.getConfig() instanceof ContentConfig) {
+			list.add(new FileCacheOnErrorListener(Config.Spider_ErrorDir + global.getTaskname()));
+		}
+
 		return list;
 	}
 
 	private PageProcessor getPageProcessor(SpiderConf conf) {
 		PageProcessor pp = null;
-		
+
 		BaseConfig pageProConf = conf.getConfig();
 		if (pageProConf instanceof PreConfig) {
 			pp = new PreListPageProcess(conf);
-		}
-		else if (pageProConf instanceof ListConfig) {
+		} else if (pageProConf instanceof ListConfig) {
 			pp = new ListPageProcess(conf);
-		}
-		else if (pageProConf instanceof ContentConfig) {
+		} else if (pageProConf instanceof ContentConfig) {
 			pp = new ContentPageProcess(conf);
 		}
-		
+
 		return pp;
 	}
-	
+
 	public void startSpiders() {
 		chain.startSpiders(true);
 	}
+	
+	private Function<Spider, SpiderConf> readUrlFromConf = new Function<Spider, SpiderConf>() {
+		@Override
+		public List<String> apply(Spider t, SpiderConf e) {
+			ListConfig config = (ListConfig) e.getConfig();
+			return config.getStarturls();
+		}
+	};
+	
+	//在适当的某处考虑利用SQL断点恢复的问题
+	
+	//TODO 在这一块考虑去重的问题
+	private Function<Spider, SpiderConf> readUrlFromSql = new Function<Spider, SpiderConf>() {
+		@Override
+		public List<String> apply(Spider t, SpiderConf e) {
+			SpiderConf config=SpiderUtil.getSpiderConfByName(e.getDependname(), getConfs());
+			return SqlUtil.getTargetUrls(config);
+		}
+	};
+	
+	private List<SpiderConf> getConfs() {
+		return confs;
+	}
 
 	public static void main(String[] args) {
-		String path = null;
-		
+		String path = "configs\\wanfangpaper.xml";
+
 		BuildSpiderChain bsc = new BuildSpiderChain(path);
 		bsc.startSpiders();
 	}
-	
+
 }
